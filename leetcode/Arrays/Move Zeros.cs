5bdb7ebe2af1ae0f01471898c1f70646d93e82c7
Milan Rathod <milanrathod945@gmail.com>
2024-09-10 00:38:06 +0530

    Move Zeros

---
+++ leetcode/Arrays/Move Zeros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays
+{
+    internal class Move_Zeros
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    int pos = i;
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[j] != 0)
+                        {
+                            pos = j;
+                            break;
+                        }
+                    }
+                    nums[i] = nums[pos];
+                    nums[pos] = 0;
+                }
+            }
+        }
+    }
+}
