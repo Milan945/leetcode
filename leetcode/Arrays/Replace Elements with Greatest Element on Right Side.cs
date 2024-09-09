a4e92217e4680d95067fd8b5c4a70bc05743bb51
Milan Rathod <milanrathod945@gmail.com>
2024-09-10 00:15:37 +0530

    Replace Elements with Greatest Element on Right Side

---
+++ leetcode/Arrays/Replace Elements with Greatest Element on Right Side.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays
+{
+    internal class Replace_Elements_with_Greatest_Element_on_Right_Side
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        arr[i] = arr[j];
+                    }
+                }
+            }
+            arr[arr.Length - 1] = -1;
+            return arr;
+        }
+    }
+}
