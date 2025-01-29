6f98af156b5b9501190cd8946d354b2b7c7cd6f7
Milan Rathod <milanrathod945@gmail.com>
2025-01-30 00:52:21 +0530

    sort array by parity

---
+++ leetcode/Arrays/Sort Array By Parity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays
+{
+    internal class Sort_Array_By_Parity
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int oddNumberPos = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[oddNumberPos];
+                    nums[oddNumberPos] = temp;
+                    oddNumberPos++;
+                }
+            }
+            return nums;
+        }
+    }
+}
